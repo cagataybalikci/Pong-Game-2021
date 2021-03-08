fab021c193acc4cf6eee2ff8931efdfc48fe33cc
cagataybalikci <cagataybalikci@gmail.com>
2021-03-08 22:11:00 +0300

    Screen dimesions created.

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
+from turtle import Turtle, Screen
+
 # TODO : Create Game Scene
+screen = Screen()
+screen.setup(width=800, height=600)
+screen.bgcolor("black")
+screen.title("Pong Game")
+
+
 # TODO : Create Paddles
 # TODO : Create Ball
 # TODO : Collision with Elements (Ball, Paddles, Screen)
 # TODO : Create Score UI and Track Score
 
+
+screen.exitonclick()
